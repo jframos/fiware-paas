d0cdd2ad59acb3bb0e9cc041c247826acefd8943
Henar Munoz <henar@tid.es>
2013-10-17 19:35:15 +0200

    license

--- model/src/main/java/com/telefonica/euro_iaas/paasmanager/model/dto/NetworkDto.java
+++ model/src/main/java/com/telefonica/euro_iaas/paasmanager/model/dto/NetworkDto.java
@@ -1,3 +1,9 @@
+/**
+ * (c) Copyright 2013 Telefonica, I+D. Printed in Spain (Europe). All Rights Reserved.<br>
+ * The copyright to the software program(s) is property of Telefonica I+D. The program(s) may be used and or copied only
+ * with the express written consent of Telefonica I+D or in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ */
 package com.telefonica.euro_iaas.paasmanager.model.dto;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -10,7 +16,7 @@ import com.telefonica.euro_iaas.paasmanager.model.SubNetwork;
 /**
  * Represents the data about the network to be invoke in the request.
  * 
- * @author Henar Muñoz
+ * @author Henar Munoz
  * @version $Id: $
  */
 @XmlRootElement
