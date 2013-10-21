1b2abe397e90bed108365288f5e1ecf321497e4f
Jesus Perez <jesuspg@tid.es>
2013-10-21 10:49:14 +0200

    mvn license:remove; mvn license:format

--- core/src/test/java/com/telefonica/euro_iaas/paasmanager/manager/impl/InfrastructureManagerImplTest.java
+++ core/src/test/java/com/telefonica/euro_iaas/paasmanager/manager/impl/InfrastructureManagerImplTest.java
@@ -4,6 +4,7 @@
  * with the express written consent of Telefonica I+D or in accordance with the terms and conditions stipulated in the
  * agreement/contract under which the program(s) have been supplied.
  */
+
 package com.telefonica.euro_iaas.paasmanager.manager.impl;
 
 import static org.mockito.Matchers.any;
