7a2ee09a99676e7063fd6fd8071507dcfd76ca67
Henar Munoz <henar@tid.es>
2014-02-26 15:36:08 +0100

    change description name longitud

--- model/src/main/java/com/telefonica/euro_iaas/paasmanager/model/EnvironmentInstance.java
+++ model/src/main/java/com/telefonica/euro_iaas/paasmanager/model/EnvironmentInstance.java
@@ -50,7 +50,9 @@ public class EnvironmentInstance extends InstallableInstance {
     @ManyToOne()
     private Environment environment;
 
+    @Column(length = 256)
     private String description;
+    @Column(nullable = false, length = 256)
     private String blueprintName = "";
     private String taskId = "";
 
