7118cbf377d91e2dd109acf9f30f84b3c6be755d
Jesus Perez <jesuspg@tid.es>
2014-05-28 18:52:05 +0200

    add rollingPolicy to logback; fix tests, because after last merge were broken; remove all System.out.println

--- model/src/test/java/com/telefonica/euro_iaas/paasmanager/model/EnvironmentTest.java
+++ model/src/test/java/com/telefonica/euro_iaas/paasmanager/model/EnvironmentTest.java
@@ -25,22 +25,21 @@
 package com.telefonica.euro_iaas.paasmanager.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.Map.Entry;
 
-import com.telefonica.euro_iaas.paasmanager.model.dto.EnvironmentDto;
 import junit.framework.TestCase;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.telefonica.euro_iaas.paasmanager.model.dto.EnvironmentDto;
+
 public class EnvironmentTest extends TestCase {
-	
+
     private static String VDC = "vdc";
-    private static String REGION ="region";
+    private static String REGION = "region";
 
     @Override
     @Before
@@ -64,19 +63,18 @@ public class EnvironmentTest extends TestCase {
         tier.setFloatingip("true");
         tier.setImage("image");
 
-        Tier tier2 = new Tier("name", new Integer(1), new Integer(1), new Integer(1), productReleases, "4",
-                "image2", "icone2");
+        Tier tier2 = new Tier("name", new Integer(1), new Integer(1), new Integer(1), productReleases, "4", "image2",
+                "icone2");
 
         Set<Tier> tiers = new HashSet<Tier>();
-        System.out.println (tiers.add(tier));
-        System.out.println (tiers.add(tier2));
+        tiers.add(tier);
+        tiers.add(tier2);
 
         Environment envResult = new Environment();
-        envResult = new Environment();
         envResult.setName("environemntName");
         envResult.addTier(tier2);
         envResult.addTier(tier);
-       // envResult.setTiers(tiers);
+        // envResult.setTiers(tiers);
 
         EnvironmentDto envDto = envResult.toDto();
         Environment env2 = envDto.fromDto();
@@ -84,70 +82,67 @@ public class EnvironmentTest extends TestCase {
 
         assertEquals(envResult.getTiers().size(), 2);
 
-        
-
     }
+
     @Test
     public void testIsDifferentRegions() {
-    	Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), null);
+        Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), null);
         tier.setRegion("region1");
-        
+
         Tier tier2 = new Tier("name2", new Integer(1), new Integer(5), new Integer(1), null);
-        tier.setRegion("region2");    
+        tier.setRegion("region2");
 
         Set<Tier> tiers = new HashSet<Tier>();
         tiers.add(tier);
         tiers.add(tier2);
 
         Environment env = new Environment();
-        
-        env.setName("environemntName");
+
+        env.setName("environmentName");
         env.addTier(tier);
         env.addTier(tier2);
-        
+
         boolean result = env.isDifferentRegions();
-        assertEquals (result, true);
+        assertEquals(result, true);
     }
-    
+
     @Test
     public void testIsNotDifferentRegions() {
-    	Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), null);
+        Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), null);
         tier.setRegion("region1");
-        
+
         Tier tier2 = new Tier("name2", new Integer(1), new Integer(5), new Integer(1), null);
-        tier2.setRegion("region1");    
+        tier2.setRegion("region1");
 
         Set<Tier> tiers = new HashSet<Tier>();
         tiers.add(tier);
         tiers.add(tier2);
 
         Environment env = new Environment();
-        
-        env.setName("environemntName");
+
+        env.setName("environmentName");
         env.addTier(tier);
         env.addTier(tier2);
-        
+
         boolean result = env.isDifferentRegions();
-        assertEquals (result, false);
-        
-        
+        assertEquals(result, false);
+
     }
-    
-    
+
     @Test
-    public void testIsNetworkFederated () {
-    	
-    	ProductRelease productRelease = new ProductRelease("product", "2.0");
+    public void testIsNetworkFederated() {
+
+        ProductRelease productRelease = new ProductRelease("product", "2.0");
         List<ProductRelease> productReleases = new ArrayList<ProductRelease>();
         productReleases.add(productRelease);
-    	
-    	Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), productReleases);
+
+        Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), productReleases);
         tier.setRegion("region1");
-        tier.addNetwork(new Network ("uno", VDC, REGION));
-        
+        tier.addNetwork(new Network("uno", VDC, REGION));
+
         Tier tier2 = new Tier("name2", new Integer(1), new Integer(5), new Integer(1), productReleases);
         tier2.setRegion("region2");
-        tier2.addNetwork(new Network ("uno", VDC, REGION));
+        tier2.addNetwork(new Network("uno", VDC, REGION));
 
         Set<Tier> tiers = new HashSet<Tier>();
         tiers.add(tier);
@@ -156,25 +151,25 @@ public class EnvironmentTest extends TestCase {
         Environment envResult = new Environment();
         envResult.setName("environemntName");
         envResult.setTiers(tiers);
-        
+
         boolean result = envResult.isNetworkFederated();
-        assertEquals (result, true);
+        assertEquals(result, true);
     }
-    
+
     @Test
-    public void testNotIsNetworkFederated () {
-    	
-    	ProductRelease productRelease = new ProductRelease("product", "2.0");
+    public void testNotIsNetworkFederated() {
+
+        ProductRelease productRelease = new ProductRelease("product", "2.0");
         List<ProductRelease> productReleases = new ArrayList<ProductRelease>();
         productReleases.add(productRelease);
-    	
-    	Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), productReleases);
+
+        Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), productReleases);
         tier.setRegion("region1");
-        tier.addNetwork(new Network ("uno2", VDC, REGION));
-        
+        tier.addNetwork(new Network("uno2", VDC, REGION));
+
         Tier tier2 = new Tier("name2", new Integer(1), new Integer(5), new Integer(1), productReleases);
         tier.setRegion("region2");
-        tier.addNetwork(new Network ("uno", VDC, REGION));
+        tier.addNetwork(new Network("uno", VDC, REGION));
 
         Set<Tier> tiers = new HashSet<Tier>();
         tiers.add(tier);
@@ -183,33 +178,33 @@ public class EnvironmentTest extends TestCase {
         Environment envResult = new Environment();
         envResult.setName("environemntName");
         envResult.setTiers(tiers);
-        
+
         boolean result = envResult.isNetworkFederated();
-        assertEquals (result, false);
+        assertEquals(result, false);
     }
-    
+
     @Test
-    public void testgetRegionNetworks () {
-    	
-    	ProductRelease productRelease = new ProductRelease("product", "2.0");
+    public void testgetRegionNetworks() {
+
+        ProductRelease productRelease = new ProductRelease("product", "2.0");
         List<ProductRelease> productReleases = new ArrayList<ProductRelease>();
         productReleases.add(productRelease);
-    	
-    	Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), productReleases);
+
+        Tier tier = new Tier("name1", new Integer(1), new Integer(5), new Integer(1), productReleases);
         tier.setRegion("region1");
-        tier.addNetwork(new Network ("uno2", VDC, REGION));
-        
+        tier.addNetwork(new Network("uno2", VDC, REGION));
+
         Tier tier3 = new Tier("name3", new Integer(1), new Integer(5), new Integer(1), productReleases);
         tier3.setRegion("region3");
-        tier3.addNetwork(new Network ("uno2", VDC, REGION));
-        
+        tier3.addNetwork(new Network("uno2", VDC, REGION));
+
         Tier tier2 = new Tier("name2", new Integer(1), new Integer(5), new Integer(1), productReleases);
         tier2.setRegion("region2");
-        tier2.addNetwork(new Network ("uno", VDC, REGION));
-        
+        tier2.addNetwork(new Network("uno", VDC, REGION));
+
         Tier tier4 = new Tier("name5", new Integer(1), new Integer(5), new Integer(1), productReleases);
         tier4.setRegion("region2");
-        tier4.addNetwork(new Network ("uno2", VDC, REGION));
+        tier4.addNetwork(new Network("uno2", VDC, REGION));
 
         Set<Tier> tiers = new HashSet<Tier>();
         tiers.add(tier);
@@ -220,11 +215,10 @@ public class EnvironmentTest extends TestCase {
         Environment envResult = new Environment();
         envResult.setName("environemntName");
         envResult.setTiers(tiers);
-        
-        Set<String> nets= envResult.getFederatedNetworks  ();
-        assertEquals (nets.size(), 1);
+
+        Set<String> nets = envResult.getFederatedNetworks();
+        assertEquals(nets.size(), 1);
 
     }
-
 
 }
