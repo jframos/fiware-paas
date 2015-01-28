81eddedb661b5074b9d920fb235e9f28ca75dfa1
Fernando Lopez <fernando.lopezaguilar@telefonica.com>
2015-01-28 14:49:00 +0100

    Delete blank spaces in the import of the AsynManagerImplTest.java

--- core/src/test/java/com/telefonica/euro_iaas/paasmanager/manager/AsynManagerImplTest.java
+++ core/src/test/java/com/telefonica/euro_iaas/paasmanager/manager/AsynManagerImplTest.java
@@ -32,7 +32,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-    import java.util.List;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
