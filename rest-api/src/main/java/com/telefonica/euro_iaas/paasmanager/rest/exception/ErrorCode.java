fa78b17077a20373d4bbedff8319520b65bfb82f
Jesus M Movilla <jesus.movilla@altran.com>
2015-02-23 10:58:11 +0100

    Including error patterns to specify errors more precisely about infrastructure and productInstallator errors

--- rest-api/src/main/java/com/telefonica/euro_iaas/paasmanager/rest/exception/ErrorCode.java
+++ rest-api/src/main/java/com/telefonica/euro_iaas/paasmanager/rest/exception/ErrorCode.java
@@ -43,11 +43,14 @@ public enum ErrorCode {
     INVALID_NUMBER_INITIAL_VMS_IN_TIER(70, "Invalid Tier. Number of Initial VMs is not correct", 
             		"(.*)InvalidEntityException(.*)Error in the Number initial(.*)", 400),
     NAME_NO_VALID(41, "The name is not valid", "(.*)InvalidEntityException:(.*)", 400),
-    INFRASTRUCTURE(50, "OpenStack infrastructure failure", "(.*)InfrastructureException(.*)", 500),
+    OPENSTACK_ERROR_CREATINGSERVER (50, "Openstack error creating a server/assigning floating ip", "(.*)InfrastructureException(.*)Error interacting with OpenStack(.*)", 500),
+    INFRASTRUCTURE(51, "OpenStack infrastructure failure", "(.*)InfrastructureException(.*)", 500),
     INVALID_ENVIRONMENT(60, "Invalid Environment Instance", "(.*)InvalidEnvironmentInstanceException(.*)", 400),
     INVALID_INITIAL_NUMBER_REPLICAS(70, "The number of replicas is not valid",
     		"(.*)InvalidEntityException:(.*)The number of replicas is not valid(.*)",
     		400),
+ 	PRODUCTINSTANCE_ALREADY_EXIST (90, "ProductInstance Already Exist", "(.*)ProductInstallatorException (.*)already exists(.*)", 400),
+ 	ERROR_INSTALLING_A_PRODUCT (91, "Error installing a product ", "(.*)ProductInstallatorException (.*)Error installing product(.*)", 400),
     DEFAULT(500, "Internal PaasManager Server error", "(?s).*", 500);
 
     private final int code;
